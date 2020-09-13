b5c10c1cba8f8e30e571e4317b67ef1b3c22591e
Sachith Rukshan <rukshan@datacaptureexperts.com.au>
2020-09-13 23:19:02 +0530

    update for qpress/core v0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AppModule } from './app/app.module';
 import qpress, { QExpress, LogLevelType } from '@qpress/core';
+import 'reflect-metadata';
 
 var app: QExpress = qpress({
     JsonParser: true,
