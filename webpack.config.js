57f55e27d848fb4724465d97fe207e02f7f458ba
Sachith Rukshan <rukshan@datacaptureexperts.com.au>
2020-10-08 08:53:00 +0530

    webpack added for better build quality

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'production',
+  devtool: false,
+  optimization: {
+    minimize: true
+  },
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(process.env.OUT_DIR || 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  target: 'node',
+  externals: [
+    nodeExternals()
+  ]
+}
